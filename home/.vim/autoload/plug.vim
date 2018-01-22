afbdaf7d964c2fe1a35b1120f2b6cc0aca31bdf7
Jeff Runningen <jeffrunningen@gmail.com>
2018-01-21 23:39:01 -0500

    Fix loading of vim-plug on Windows

--- home/.vim/autoload/plug.vim
+++ home/.vim/autoload/plug.vim
@@ -1,1 +1,6 @@
-../vim-plug/plug.vim
+" This should work on both Linux and Windows. Old versions of git on Windows
+" have trouble with symlinks, so to be safe, this file sources plug.vim
+" instead of using a symlink.
+
+let g:plugpath = expand('<sfile>:h') . '/../vim-plug/plug.vim'
+execute 'source ' . g:plugpath
--- windows/InstallDotfiles.ps1
+++ windows/InstallDotfiles.ps1
@@ -1,7 +1,7 @@
 # Overwrite the current PowerShell profile, and hardlink that path to the
 # profile in this repo.
-New-Item -ItemType HardLink -Path $profile -Target "$PSScriptRoot\Microsoft.PowerShell_profile.ps1" -Force
+New-Item -ItemType SymbolicLink -Path $profile -Target "$PSScriptRoot\Microsoft.PowerShell_profile.ps1" -Force
 
 # Put in a sensible vimrc, and link the runfiles directory to ~/vimfiles.
-New-Item -ItemType HardLink -Path "$HOME/_vimrc" -Target "$PSScriptRoot\_vimrc" -Force
+New-Item -ItemType SymbolicLink -Path "$HOME/_vimrc" -Target "$PSScriptRoot\_vimrc" -Force
 New-Item -type Junction -Path "$HOME/vimfiles" -Value "$PSScriptRoot\..\home\.vim" -Force
